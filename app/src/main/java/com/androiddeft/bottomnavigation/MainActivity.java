70989f8e5f184be65d13cd7b52c30f7f80ba9361
mac <truongpham@bravesoft.com.vn>
2019-02-18 10:18:49 +0700

    Initial commit

---
+++ app/src/main/java/com/androiddeft/bottomnavigation/MainActivity.java
@@ -0,0 +1,78 @@
+package com.androiddeft.bottomnavigation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Fragment fragment;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragment = new DashboardFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_notifications:
+                    fragment = new NotificationsFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.it_trangchu:
+                    fragment = new DashboardFragment();
+                    loadFragment(fragment);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Load the HomeFragment when app is loaded
+        fragment = new HomeFragment();
+        loadFragment(fragment);
+
+        //Hide the Bottom Navigation when the page is scrolled
+        //CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
+        //layoutParams.setBehavior(new ScrollHandler());
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        //Close the application when back button is pressed
+        finish();
+    }
+}
